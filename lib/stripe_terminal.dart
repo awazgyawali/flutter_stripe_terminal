1d34174fbbdfdfabe76475d58f3b436c6551ea7a
Aawaz <awazgyawali@gmail.com>
2022-05-01 21:47:04 +0545

    Added documentation to the dart code

--- lib/stripe_terminal.dart
+++ lib/stripe_terminal.dart
@@ -12,8 +12,11 @@ part 'models/payment_method.dart';
 class StripeTerminal {
   static const MethodChannel _channel = MethodChannel('stripe_terminal');
   Future<String> Function() fetchToken;
+
+  /// Initializes the terminal SDK
   StripeTerminal({
-    // A callback that returns a Future that resolves to a connection token from your backend
+    /// A callback function that returns a Future which resolves to a connection token from your backend
+    /// Check out more at https://stripe.com/docs/terminal/payments/setup-integration#connection-token
     required this.fetchToken,
   }) {
     _channel.setMethodCallHandler((call) async {
@@ -33,6 +36,9 @@ class StripeTerminal {
     _channel.invokeMethod("init");
   }
 
+  /// Connects to a reader, only works if you have scanned devices within this session.
+  ///
+  /// Always run `discoverReaders` before calling this function
   Future<bool> connectToReader(
     StripeReader reader, {
     String? locationId,
@@ -48,6 +54,7 @@ class StripeTerminal {
     }
   }
 
+  /// Checks the connection status of the SDK
   Future<ConnectionStatus> connectionStatus() async {
     int? statusId = await _channel.invokeMethod<int>("connectionStatus");
     if (statusId == null) {
@@ -57,6 +64,7 @@ class StripeTerminal {
     }
   }
 
+  /// Fetches the connected reader from the SDK. `null` if not connected
   Future<StripeReader?> fetchConnectedReader() async {
     Map? reader = await _channel.invokeMethod<Map>("fetchConnectedReader");
     if (reader == null) {
@@ -66,6 +74,7 @@ class StripeTerminal {
     }
   }
 
+  /// Extracts payment method from the reader
   Future<StripePaymentMethod> readPaymentMethod() async {
     Map cardDetail = await _channel.invokeMethod("readPaymentMethod");
     return StripePaymentMethod.fromJson(cardDetail);
@@ -73,6 +82,12 @@ class StripeTerminal {
 
   final StreamController<List<StripeReader>> _readerStreamController =
       StreamController<List<StripeReader>>();
+
+  /// Starts scanning readers in the vicinity. This will return a list of readers.
+  ///
+  /// Can contain an empty array if no readers are found.
+  ///
+  /// [simulated] se to `true` will simulate readers which can be connected and tested.
   Stream<List<StripeReader>> discoverReaders({
     bool simulated = false,
   }) {
